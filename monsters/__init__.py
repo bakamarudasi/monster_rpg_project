60089e13a6708e6964951c19d0204b1327e7e85a
koisohiiro <62692092+bakamarudasi@users.noreply.github.com>
2025-06-05 15:20:34 +0900

    Fix monsters package init

--- monsters/__init__.py
+++ monsters/__init__.py
@@ -1,4 +1,10 @@
-# monsters/__init__.py (このファイルも `monsters` フォルダ直下に置きます)
+# monsters package initialization
 
-from .monster_class import Monster, GROWTH_TYPE_AVERAGE, GROWTH_TYPE_EARLY, GROWTH_TYPE_LATE
+from .monster_class import (
+    Monster,
+    GROWTH_TYPE_AVERAGE,
+    GROWTH_TYPE_EARLY,
+    GROWTH_TYPE_LATE,
+)
 from .monster_data import ALL_MONSTERS
+
