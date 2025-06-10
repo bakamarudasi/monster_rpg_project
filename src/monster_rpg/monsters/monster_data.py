741245fb327ca7d98c38daf5ce9d15bba371b29c
koisohiiro <62692092+bakamarudasi@users.noreply.github.com>
2025-06-11 08:20:55 +0900

    Update monster_data.py

--- src/monster_rpg/monsters/monster_data.py
+++ src/monster_rpg/monsters/monster_data.py
@@ -55,7 +55,7 @@ GOBLIN = Monster(
     growth_type=GROWTH_TYPE_AVERAGE,
     monster_id="goblin",
     rank=RANK_D, # 例: ゴブリンはDランク
-    drop_items=[(ALL_ITEMS["small_potion"], 0.2), (ALL_ITEMS["magic_stone"], 0.1)]
+    drop_items=[(ALL_ITEMS["small_potion"], 0.2), (ALL_ITEMS["magic_stone"], 0.1),(ALL_ITEMS["bronze_sword"],0.2),0]
 )
 
 
